8ae734a26b81cbd861c82377793e231ce2af0f8c
Joseph Anderson <josepha93@gmail.com>
2022-04-15 09:09:05 -0400

    step4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 }
@@ -28,15 +30,17 @@ const Header = props =>
 
 const Content = props =>
   <div>
-    <Part part={props.part1} />
-    <Part part={props.part2} />
-    <Part part={props.part3} />
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
   </div>;
 
 const Part = props =>
   <p>{props.part.name} {props.part.exercises}</p>;
 
-const Total = props =>
-  <p>Number of exercises {props.total}</p>;
+const Total = props => {
+  const total = props.parts.reduce((total, part) => total + part.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+}
 
 export default App;
